aca8e0100ec7ef842f8e863bca89938a964f1216
Ajinkya Naik <51918054+aj-naik@users.noreply.github.com>
2021-06-19 21:57:27 +0530

    Added Utils.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from keras.optimizers import Adam
 from keras.layers import MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from threading import Thread
+from utils import *
 
 emotion_model = Sequential()
 emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
@@ -39,34 +40,6 @@ global last_frame1
 last_frame1 = np.zeros((480, 640, 3), dtype=np.uint8)
 global cap1 
 show_text=[0]
-
-class WebcamVideoStream:
-    	
-		def __init__(self, src=0):
-			self.stream = cv2.VideoCapture(src,cv2.CAP_DSHOW)
-			(self.grabbed, self.frame) = self.stream.read()
-			self.stopped = False
-
-		def start(self):
-				# start the thread to read frames from the video stream
-			Thread(target=self.update, args=()).start()
-			return self
-			
-		def update(self):
-			# keep looping infinitely until the thread is stopped
-			while True:
-				# if the thread indicator variable is set, stop the thread
-				if self.stopped:
-					return
-				# otherwise, read the next frame from the stream
-				(self.grabbed, self.frame) = self.stream.read()
-
-		def read(self):
-			# return the frame most recently read
-			return self.frame
-		def stop(self):
-			# indicate that the thread should be stopped
-			self.stopped = True
 
 def web_cam(): 
     global cap1     
@@ -98,6 +71,7 @@ def web_cam():
        exit()
        
 def emoji():
+    print('---------------- Value ------------', emoji_dist[show_text[0]])
     frame2=cv2.imread(emoji_dist[show_text[0]])
     pic2=cv2.cvtColor(frame2,cv2.COLOR_BGR2RGB)
     img2=Image.fromarray(frame2)
